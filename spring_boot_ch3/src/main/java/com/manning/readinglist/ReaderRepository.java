3df92e36c5a2ebd775eb5b0521bbb68213624e90
Nguyễn Hải QQuyền <nguyenhaiquyen4@gmail.com>
2017-11-30 15:00:01 +0700

    Spring boot chapter 3 tmp

---
+++ spring_boot_ch3/src/main/java/com/manning/readinglist/ReaderRepository.java
@@ -0,0 +1,7 @@
+package com.manning.readinglist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReaderRepository
+        extends JpaRepository<Reader, String> {
+}
---
+++ spring_boot_ch3/src/main/java/com/manning/readinglist/ReadingListController.java
@@ -0,0 +1,46 @@
+package com.manning.readinglist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+    private ReadingListRepository readingListRepository;
+    private AmazonProperties amazonProperties;
+
+    @Autowired
+    public ReadingListController(
+            ReadingListRepository readingListRepository,
+            AmazonProperties amazonProperties) {
+        this.readingListRepository = readingListRepository;
+        this.amazonProperties = amazonProperties;
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String readersBooks(Reader reader, Model model) {
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonID", amazonProperties.getAssociateId());
+        }
+        return "readingList";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/{reader}";
+    }
+}
